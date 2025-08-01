ba7bf013260463b005fdac7400749bf4ede05992
mahalsanea <mahalsanea@gmail.com>
2025-08-01 21:11:43 +0300

    First project

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace vtysodevi
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginPage());
-            // Application.Run(new LoginPage());
+            // Application.Run();
         }
     }
 }
